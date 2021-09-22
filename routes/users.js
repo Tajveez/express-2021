6a7266054369d33863b5bd9cda68994666a2418f
Tajveez <Tajveez786@gmail.com>
2021-09-22 23:39:17 +0500

    Router chaining

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,18 @@ router.get('/new', (req, res) => {
   res.send('User New')
 })
 
+// new method
+router.route('/:id')
+    .get((req, res) => {
+        res.send(`Get user with id: ${id}`)
+    })
+    .put((req, res) => {
+        res.send(`Put user with id: ${id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete user with id: ${id}`)
+    })
+
 router.get('/:id', (req, res) => {
     res.send(`User with id: ${req.params.id}`)
 })
