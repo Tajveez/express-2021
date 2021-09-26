756b77ea8d4c711c779d3a55f08c0f61bdfb2940
Tajveez <Tajveez786@gmail.com>
2021-09-26 20:50:09 +0500

    Code done

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ const app = express();
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-// app.get("/", logger, logger, logger, (req, res) => {
-//   console.log("Hello");
-
-//   // res.status(403).json({
-//   //   status: 403,
-//   //   message: "Access to this page forbidden",
-//   // });
-
-//   // res.download("package.json");
-
-//   res.render("index", {
-//     text: "World",
-//   });
-//   // res.send("Hello");
-// });
-
 app.use(logger)
 
 const userRouter = require('./routes/users')
